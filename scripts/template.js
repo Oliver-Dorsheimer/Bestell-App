6f47b84ecdfcc2749a95226f576dfd14d043e85c
Oliver Dorsheimer <dorsheimer.oliver@gmx.de>
2026-03-24 18:26:21 +0100

    added and designed order basket

--- scripts/template.js
+++ scripts/template.js
@@ -3,11 +3,11 @@ function dishTemplate(index){
         <article class = "dish_card">
             <img class = "dish_img" src = "./assets/img/${dishes[index].picture}.jpg">
             <div class = "dish_info">
-                <h3>${dishes[index].name}</h3>
+                <h4>${dishes[index].name}</h4>
                 <p>${dishes[index].description}</p>
             </div>
             <div class = "dish_price_and_order">
-                <h3>${dishes[index].price}</h3>
+                <h4>${dishes[index].price}</h4>
                 <button class = "dish_order_button">Add to Basket</button>
             </div>
         </article>    
