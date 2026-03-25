8ed58e879de9c8b049ae1fc6fd36c4c1547eb48a
Oliver Dorsheimer <dorsheimer.oliver@gmx.de>
2026-03-25 19:48:59 +0100

    startet working  on basket functionality

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function renderElements(){
+    setIds();
     renderElementsFromCategory("burger_and_sandwiches_content");
     renderElementsFromCategory("pizza_content");
     renderElementsFromCategory("salad_content");
+};
+
+function setIds(){
+    for(let i = 0; i < dishes.length; i++){
+        dishes[i].id = i;
+    };
 };
 
 function renderElementsFromCategory(category){
@@ -13,4 +20,59 @@ function renderElementsFromCategory(category){
         };
     };
 };
+/*
+function renderBasketContents(index){
+    if(basket.length != null){
+        for(let i = 0; i < basket.length; i++){
+          document.getElementById("basket_content").innerHTML +=  basketContentTemplate(i);
+        };
+    }else{
 
+    };
+};
+*/
+function renderElementFromBasket(menuID){
+    document.getElementById("basket_content").innerHTML += basketContentTemplate(menuID);
+};
+
+function regenerateElementFromBasket(index){
+    dishes[index].amount = Number(dishes[index].amount) + 1;
+    document.getElementById(`title_amount${index}`).innerHTML = basketTitleTemplate(index);
+    document.getElementById(`add_and_remove_amount${index}`).innerHTML = dishes[index].amount;
+};
+
+function addToBasket(index, menuID){
+    if(dishes[index].inBasket == false){
+        dishes[index].inBasket = true;
+        basket.push(dishes[index]);
+        renderElementFromBasket(menuID);
+        document.getElementById(`order_element_delete_icon${index}`).setAttribute("src","./assets/icon/delete_icon.png");
+        console.log(basket);
+    }else{
+        let currentElementIndex = basket.findIndex(basketElement => basketElement.id == menuID);
+        document.getElementById(`order_element_delete_icon${index}`).setAttribute("src","./assets/icon/minus_icon.png");
+        regenerateElementFromBasket(index);
+    };
+};
+
+function suptractFromBasket(){
+
+};
+
+function removeFromBasket(){
+
+};
+
+/*
+if(dishes[index].inBasket == false){
+        dishes[index].inBasket = true;
+        document.getElementById("basket_content").innerHTML += basketContentTemplate(index);
+        document.getElementById(`order_element_delete_icon${index}`).setAttribute("src","./assets/icon/delete_icon.png")
+    }else if (Number(dishes[index].amount) > 0){
+        document.getElementById(`order_element_delete_icon${index}`).setAttribute("src", "./assets/icon/minus_icon.png")
+        regenerateElementFromBasket(index);
+    }else{
+        regenerateElementFromBasket(index);
+    };
+
+*/
--- scripts/template.js
+++ scripts/template.js
@@ -8,8 +8,31 @@ function dishTemplate(index){
             </div>
             <div class = "dish_price_and_order">
                 <h4>${dishes[index].price}</h4>
-                <button class = "dish_order_button">Add to Basket</button>
+                <button class = "dish_order_button" onclick = "addToBasket(${index}, ${dishes[index].id})">Add to Basket</button>
             </div>
-        </article>    
+        </article>
     `
 };
+
+function basketContentTemplate(index){
+    return `<div class = "order_element_container">
+        <div class = "order_element_title">
+            <p class = "order_title" id = "title_amount${index}">${dishes[index].amount} x <span>${dishes[index].name}</span></p>
+        </div/
+        <div class = "order_element_price">
+            <div class = "price_control">
+                <div class = "add_and_remove_container">
+                    <img id = "order_element_delete_icon${index}">
+                    <p id = "add_and_remove_amount${index}">${dishes[index].amount}</p>
+                    <img class = "order_element_delete_icon" src = "./assets/icon/add_icon.png">
+                </div>
+                <p>${Number(dishes[index].price) * Number(dishes[index].amount)}€</p>
+            </div>
+        </div>
+    </div>
+    `
+};
+
+function basketTitleTemplate(index){
+    return `${dishes[index].amount} x <span>${dishes[index].name}</span>`
+}
